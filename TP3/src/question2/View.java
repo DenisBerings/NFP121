c20eccb8a8062b2490535a8daedb1ceec0e8cbfb
DenisBerings <50744470+DenisBerings@users.noreply.github.com>
2021-06-24 19:49:21 +0200

    Update View.java

--- TP3/src/question2/View.java
+++ TP3/src/question2/View.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.util.Observable;
 import java.util.Observer;
 
-// On implémente la classe java Observer
+// On implémente l'interface java Observer afin que la classe View hérite de ses méthodes
 public class View implements Observer {
     private JTextField jTextField;
     private JButton jButton1;
@@ -46,9 +46,9 @@ public class View implements Observer {
     // On génère & surcharge la méthode update héritée de la classe java Observer
     @Override
     public void update(Observable observable, Object o) {
-        // Si la variable "observable" est un nombre...
+        // Si la variable "observable" est une instance de Number...
         if(observable instanceof Number) {
-            // ...on récupère la valeur transmise et on s'assure qu'elle est converti en nombre...
+            // ...on récupère la valeur transmise et on s'assure qu'elle est convertie en Number...
             Number number = (Number) observable;
             // ...et on l'assigne à notre champ texte
             this.jTextField.setText(String.valueOf(number.getValue()));
