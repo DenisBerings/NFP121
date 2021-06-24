dab147bb239c54d530b7e0256313431eff5a06da
DenisBerings <50744470+DenisBerings@users.noreply.github.com>
2021-06-24 18:57:00 +0200

    Update Broker.java

--- TP3/src/question1/Broker.java
+++ TP3/src/question1/Broker.java
@@ -1,5 +1,5 @@
 package question1;
-// On implémente l'interface Observer sur la classe Broker
+// On implémente l'interface Observer sur la classe Broker afin que cette dernière hérite de sa méthode update()
 public class Broker implements Observer{
     // On déclare la variable changeDetected (booléen)
     private boolean changeDetected;
@@ -7,7 +7,7 @@ public class Broker implements Observer{
     public boolean isChangeDetected() {
         return changeDetected;
     }
-    // On surcharge la méthode update héritée de l'interface Observer
+    // On surcharge la méthode update héritée de l'interface Observer pour qu'elle affecte la valeur true à la variable changeDetected 
     @Override
     public void update() {
         this.changeDetected = true;
