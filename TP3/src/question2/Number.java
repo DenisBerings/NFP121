fa1014bd181ef0850ad4983c8fd0c2abb33d1f8f
DenisBerings <50744470+DenisBerings@users.noreply.github.com>
2021-06-24 19:44:14 +0200

    Update Number.java

--- TP3/src/question2/Number.java
+++ TP3/src/question2/Number.java
@@ -6,13 +6,14 @@ import java.util.Observable;
 public class Number extends Observable {
     // On déclare une variable "value" (entier)
     private int value;
-    // On génère les Getters & Setters pour cette variable
+    // On génère le Getter...
     public int getValue() {
         return value;
     }
-
+    // ...& Setter pour la variable value...
     public void setValue(int value) {
         this.value = value;
+        // ...Et on appelle les méthodes setChanged() & notifyObservers() de la classe mère (Observable)
         setChanged();
         notifyObservers();
     }
