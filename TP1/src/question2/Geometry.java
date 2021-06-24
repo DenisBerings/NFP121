57fa31c6bda7b9d5fe92a1c18e3fb0d3a705797c
DenisBerings <50744470+DenisBerings@users.noreply.github.com>
2021-06-24 15:55:53 +0200

    Update Geometry.java

--- TP1/src/question2/Geometry.java
+++ TP1/src/question2/Geometry.java
@@ -3,19 +3,21 @@ package question2;
 import java.util.Objects;
 
 public class Geometry {
-    // On passe la variable type en private
+    // On passe la variable type en private (testcheckFieldVisibility())
     private String type;
-    // On crée une variable color (String)
+    // On déclare une variable color (String) (test checkFieldIsDeclared())
     private String color;
-    // On crée une fonction surface qui prend 2 int en paramètre et qui retourne le produit de ces 2 paramètres
+    /* On crée une fonction surface qui prend 2 int en paramètre et qui retourne 
+       le produit de ces 2 paramètres (test checkSurfaceCalculation()) */
     public static int surface (int a, int b) {
         return a*b;
     }
-    // On crée une fonction perimeter qui prend 1 int en paramètre et qui ce paramètre * 4
+    /* On crée une fonction perimeter qui prend 1 int en paramètre et qui 
+       multiplie ce paramètre par 4 (test checkPerimeterCalculation()) */
     public static int perimeter (int a) {
         return a*4;
     }
-    // On génère les getters & setters pour la variable type
+    // On génère les getters & setters pour la variable type (facultatif)
     public String getType() {
         return type;
     }
@@ -23,12 +25,13 @@ public class Geometry {
     public void setType(String type) {
         this.type = type;
     }
-    // On génère la fonction toString() et la surcharger pour qu'elle renvoie le String qui va bien
+    /* On génère la fonction toString() et la surcharger pour qu'elle renvoie 
+       le String qui va bien (test checkToStringMethod()) */
     @Override
     public String toString() {
         return "Type:"+type;
     }
-    // On surcharge la fonction equals()
+    // On surcharge la fonction equals() (test checkEquality())
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
