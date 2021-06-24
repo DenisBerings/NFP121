8bb30d9d5f13dcd7295d91cadfe09d67022f82b7
DenisBerings <50744470+DenisBerings@users.noreply.github.com>
2021-06-24 17:05:37 +0200

    Update PileAdaptor.java

--- TP2/src/question1/PileAdaptor.java
+++ TP2/src/question1/PileAdaptor.java
@@ -1,23 +1,26 @@
 package question1;
 // On implémente l'interface PileI sur la class PileAdaptor (test checkInstanceType())
 public class PileAdaptor implements PileI {
-    // On instancie un objet StackImpl (test checkInstanceType())
+    // On instancie une objet de type StackImpl (test checkInstanceType())
     public StackImpl stack;
     // On crée un constructeur à 1 paramètre (1 objet StackImpl) (test checkInstanceType())
     PileAdaptor(StackImpl stack) {
         this.stack = stack;
     }
-    // On surcharge la méthode empiler, héritée de l'interface PileI (test testEmpiler())
+    /* On surcharge la méthode empiler, héritée de l'interface PileI, pour la faire 
+       correspondre à la méthode push() de la classe StackIml (test testEmpiler()) */
     @Override
     public void empiler(Object o) {
       this.stack.push(o);
     }
-    // On surcharge la méthode depiler, héritée de l'interface PileI (test testDepile())
+    /* On surcharge la méthode depiler, héritée de l'interface PileI, pour la faire 
+       correspondre à la méthode pop() de la classe StackIml (test testDepile()) */
     @Override
     public Object depiler() {
         return this.stack.pop();
     }
-    // On surcharge la méthode estVide, héritée de l'interface PileI (test testEstVide())
+    /* On surcharge la méthode estVide, héritée de l'interface PileI, pour la faire 
+       correspondre à la méthode isEmpty() de la classe StackIml (test testEstVide()) */
     @Override
     public boolean estVide() {
         return this.stack.isEmpty();
