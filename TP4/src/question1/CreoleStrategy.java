bdb85a46efcdbea2aee097c5fb0aa16e7937ca0d
DenisBerings <50744470+DenisBerings@users.noreply.github.com>
2021-06-24 21:31:48 +0200

    Update CreoleStrategy.java

--- TP4/src/question1/CreoleStrategy.java
+++ TP4/src/question1/CreoleStrategy.java
@@ -1,7 +1,7 @@
 package question1;
-// On implémente l'interface Strategy sur la classe CreoleStrategy
+// On implémente l'interface Strategy sur la classe CreoleStrategy pour que cette dernière hérite de sa méthode cookRice()
 public class CreoleStrategy implements Strategy {
-    // On ajoute un type de retour "RiceCookingWays et on retourne le bon type de "RiceCookingWays"
+    // On surcharge la méthode cookRice héritée de l'interface Strategy... 
     @Override
     public RiceCookingWays cookRice() {
         System.out.println("In a small saucepan, melt butter.");
@@ -9,6 +9,7 @@ public class CreoleStrategy implements Strategy {
         System.out.println("Cook over medium heat for 3 minutes.");
         System.out.println("Stir in rice.");
         System.out.println("Cover and heat through.");
+        // ...et on lui fait retourner l'enum RiceCookingWays.CREOLE
         return RiceCookingWays.CREOLE;
     }
 
