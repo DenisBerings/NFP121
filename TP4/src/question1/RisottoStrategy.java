ec98f64b1dc4ca6b26bae346096a52809bcffd69
DenisBerings <50744470+DenisBerings@users.noreply.github.com>
2021-06-24 21:32:06 +0200

    Update RisottoStrategy.java

--- TP4/src/question1/RisottoStrategy.java
+++ TP4/src/question1/RisottoStrategy.java
@@ -1,13 +1,13 @@
 package question1;
-// On implémente l'interface Strategy sur la classe RisottoStrategy
+// On implémente l'interface Strategy sur la classe RisottoStrategy pour que cette dernière hérite de sa méthode cookRice() 
 public class RisottoStrategy implements Strategy{
-    // On ajoute un type de retour "RiceCookingWays et on retourne le bon type de "RiceCookingWays"
-    @Override
+    // On surcharge la méthode cookRice héritée de l'interface Strategy... 
     public RiceCookingWays cookRice() {
         System.out.println("Use Cold Stock. Adding chilly stock to a hot pan will cool everything down and mess up the cooking process.");
         System.out.println("Stir It Constantly (or Not at All)");
         System.out.println("Add Too Much Stock.");
         System.out.println("Cook the Rice Till It's Mushy.");
+        // et on lui fait retourner l'enum RiceCookingWays.RISOTTO
         return RiceCookingWays.RISOTTO;
     }
 }
