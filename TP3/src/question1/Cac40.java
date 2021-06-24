b48d81d67814b826379fdb17d7b4e76238a44ce1
DenisBerings <50744470+DenisBerings@users.noreply.github.com>
2021-06-24 18:57:28 +0200

    Update Cac40.java

--- TP3/src/question1/Cac40.java
+++ TP3/src/question1/Cac40.java
@@ -1,15 +1,16 @@
 package question1;
-
+//On étend la classe abstraite Observable à la classe Cac40 afin que cette dernière hérite de ses méthodes
 public class Cac40 extends Observable {
-    // On déclare une variable value
+    // On déclare un entier value
     private int value;
-    // On génère les Getter & Setter pour la variable value
+    // On génère le Getter...
     public int getValue() {
         return value;
     }
-
+    // ...& Setter pour la variable value...
     public void setValue(int value) {
         this.value = value;
+        // ...Et on appelle la méthode notifyObs() de la classe mère (Observable)
         super.notifyObs();
     }
 }
