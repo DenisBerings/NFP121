aaf1cb80a11419f47d5ac0dd901e9d2675c0390d
DenisBerings <50744470+DenisBerings@users.noreply.github.com>
2021-06-24 18:57:07 +0200

    Update Observable.java

--- TP3/src/question1/Observable.java
+++ TP3/src/question1/Observable.java
@@ -6,30 +6,26 @@ import java.util.List;
 public abstract class Observable {
     // On déclare une liste d'Observer
     private List<Observer> observerList;
-    // On crée un constructuer
+    // On crée un constructeur qui va instancier la liste observerList (et qui sera le constructeur par defaut de la classe fille "Cac40")
     Observable() {
-        //on instancie la liste d'observer
         this.observerList = new ArrayList<>();
     }
-    // On crée une méthode add
+    // On crée une méthode add qui prend un objet de type Observer en paramètre et l'ajoute à la liste observerList
     public void add(Observer o) {
         this.observerList.add(o);
     }
-    // On crée une méthode remove
+    // On crée une méthode remove qui prend un objet de type Observer en paramètre et le retire de la liste observerList
     public void remove(Observer o) {
         this.observerList.remove(o);
     }
-    // On crée une méthode getObserverCount
+    // On crée une méthode getObserverCount qui retourne la taille de la liste observerList
     public int getObserverCount() {
         return this.observerList.size();
     }
-    // On crée une méthode notifyObs
+    // On crée une méthode notifyObs qui appelle la méthode update() sur chaque observer de la liste observerList
     public void notifyObs() {
         for (Observer o: this.observerList) {
             o.update();
         }
     }
-
-
-
 }
