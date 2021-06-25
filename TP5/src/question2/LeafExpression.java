a8246606cb9b5ccd239784fda808543fbb9cd38f
DenisBerings <50744470+DenisBerings@users.noreply.github.com>
2021-06-25 16:10:23 +0200

    Update LeafExpression.java

--- TP5/src/question2/LeafExpression.java
+++ TP5/src/question2/LeafExpression.java
@@ -1,5 +1,19 @@
 package question2;
-
-//public class LeafExpression implements Expression {
-//
-//}
+// On implémente l'interface Expression pour que celle-ci hérite de sa méthode interpreter()
+public class LeafExpression implements Expression {
+    // On déclare un entier value
+    private int value;
+    // On crée un constructeur qui prend l'entier en paramètre et l'instancie
+    public LeafExpression(int value) {
+        this.value = value;
+    }
+    // On génère le getter de l'entier instancié
+    public int getValue() {
+        return value;
+    }
+    // On surcharge la méthode interpreter() hétitée de l'interface Expression pour qu'elle retourne l'entier instancié
+    @Override
+    public int interpreter() {
+        return value;
+    }
+}
