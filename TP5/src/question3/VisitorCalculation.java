fe284d837e864cbbc5fb66a54beef23bdf001eeb
DenisBerings <50744470+DenisBerings@users.noreply.github.com>
2021-06-25 16:52:46 +0200

    Update VisitorCalculation.java

--- TP5/src/question3/VisitorCalculation.java
+++ TP5/src/question3/VisitorCalculation.java
@@ -7,8 +7,8 @@ public class VisitorCalculation extends Visitor<Integer> {
     public Integer visit(LeafExpression n) {
         return n.getValue();
     }
-    /* On surcharge la méthode visite (Substraction) pour qu'elle retourne 
-        un appel à la méthode getValue sur l'objet de type Substraction */
+    /* On surcharge la méthode visite (Substraction) pour qu'elle retourne l'opération des appels 
+        aux méthode accept() sur les objets Epression de l'objet Substraction passé en paramètre */
     @Override
     public Integer visit(Substraction s) {
         return s.op1.accept(this) - s.op2.accept(this);
