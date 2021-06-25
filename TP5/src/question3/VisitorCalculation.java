5558dee6a89a5a490da822e638ae969ccb0407c2
DenisBerings <50744470+DenisBerings@users.noreply.github.com>
2021-06-25 16:50:14 +0200

    Update VisitorCalculation.java

--- TP5/src/question3/VisitorCalculation.java
+++ TP5/src/question3/VisitorCalculation.java
@@ -1,5 +1,16 @@
 package question3;
-
+// On étend la classe abstraite Visitor() à cette classe pour que celle-ci hérite de ses méthodes visit()
 public class VisitorCalculation extends Visitor<Integer> {
-
+    /* On surcharge la méthode visite (LeafExpression) pour qu'elle retourne 
+        un appel à la méthode getValue sur l'objet de type LeafExpression */
+    @Override
+    public Integer visit(LeafExpression n) {
+        return n.getValue();
+    }
+    /* On surcharge la méthode visite (Substraction) pour qu'elle retourne 
+        un appel à la méthode getValue sur l'objet de type Substraction */
+    @Override
+    public Integer visit(Substraction s) {
+        return s.op1.accept(this) - s.op2.accept(this);
+    }
 }
