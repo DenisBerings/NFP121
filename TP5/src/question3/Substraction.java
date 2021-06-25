4cc91bc3b80fa3f97ef8bcd978fe670d6c1cd757
DenisBerings <50744470+DenisBerings@users.noreply.github.com>
2021-06-25 16:41:38 +0200

    Update Substraction.java

--- TP5/src/question3/Substraction.java
+++ TP5/src/question3/Substraction.java
@@ -1,6 +1,19 @@
 package question3;
 
-//public class Substraction implements Expression {
-//
-//}
-//
+// On implémente l'interface Expression sur cette classe pour que celle-ci hérite de sa méthode interpreter()
+public class Substraction implements Expression {
+    // On déclare 2 objets de type Expression
+    Expression op1;
+    Expression op2;
+    // On crée un constructeur qui prend les 2 objets Expression en paramètre et les instancie
+    public Substraction(Expression op1, Expression op2) {
+        this.op1 = op1;
+        this.op2 = op2;
+    }
+    /* On surcharge la méthode abstraite accept() héritée de l'interface Expression pour qu'elle 
+       retourne un appel à la méthode visit() sur la liste de Visitor passée en paramaère */
+    @Override
+    public <T> T accept(Visitor<T> v) {
+        return v.visit(this);
+    }
+}
