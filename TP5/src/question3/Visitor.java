100467df99fcfdeab4fbf73e4b211e0956abdf01
DenisBerings <50744470+DenisBerings@users.noreply.github.com>
2021-06-25 16:44:26 +0200

    Update Visitor.java

--- TP5/src/question3/Visitor.java
+++ TP5/src/question3/Visitor.java
@@ -1,5 +1,8 @@
 package question3;
 
 public abstract class Visitor<T> {
-
+    /* On impose 2 méthodes visit ayant une signature différente (polymorphisme ad-hoc)
+       aux classes filles qui étendront cette classe abstraite */
+    public abstract T visit(LeafExpression n);
+    public abstract T visit(Substraction s);
 }
