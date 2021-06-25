c7a28dec959b23e5205209066d6c690c95ee60d4
DenisBerings <50744470+DenisBerings@users.noreply.github.com>
2021-06-25 11:01:41 +0200

    Update Cook.java

--- TP4/src/question1/Cook.java
+++ TP4/src/question1/Cook.java
@@ -1,16 +1,18 @@
 package question1;
 
-// On implémente l'interface Strategy sur la classe Cook
+// On implémente l'interface Strategy sur la classe Cook pour que cette dernière hérite de sa méthode cookRice()
 public class Cook implements Strategy {
     // On déclare une variable de type Strategy
     private Strategy strategy ;
-    // On surcharge la méthode cookRice héritée de l'interface Strategy
-    @Override
-    public RiceCookingWays cookRice() {
-        return strategy.cookRice();
-    }
+    
     // On génère le Setter de la variable strategy précédemment déclarée
     public void setStrategy(Strategy strategy) {
         this.strategy = strategy;
+    }
+    /* On surcharge la méthode cookRice héritée de l'interface Strategy pour lui faire  
+       retourner un appel à la méthode cookRice() sur l'objet strategy instancié */
+    @Override
+    public RiceCookingWays cookRice() {
+        return strategy.cookRice();
     }
 }
