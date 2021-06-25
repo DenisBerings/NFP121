f4195ac961e1293bb3dfa34d6aaec2b346f7ade3
DenisBerings <50744470+DenisBerings@users.noreply.github.com>
2021-06-25 14:58:37 +0200

    Update Song.java

--- TP5/src/question1/Song.java
+++ TP5/src/question1/Song.java
@@ -11,22 +11,22 @@ public class Song extends SongComponent {
         this.bandName = bandName;
         this.releaseYear = releaseYear;
     }
-    // On la méthode getSongName() héritée de la classe abstraite SongComponent, pour lui faire retourner la variable songName instanciée
+    // On surcharge la méthode getSongName() héritée de la classe abstraite SongComponent, pour lui faire retourner la variable songName instanciée
     @Override
     public String getSongName() {
         return songName;
     }
-    // On la méthode getBandName() héritée de la classe abstraite SongComponent, pour lui faire retourner la variable bandName instanciée
+    // On surcharge la méthode getBandName() héritée de la classe abstraite SongComponent, pour lui faire retourner la variable bandName instanciée
     @Override
     public String getBandName() {
         return bandName;
     }
-    // On la méthode getReleaseYear() héritée de la classe abstraite SongComponent, pour lui faire retourner la variable releaseYear instanciée
+    // On surcharge la méthode getReleaseYear() héritée de la classe abstraite SongComponent, pour lui faire retourner la variable releaseYear instanciée
     @Override
     public int getReleaseYear() {
         return releaseYear;
     }
-    // On la méthode displaySongInfo() héritée de la classe abstraite SongComponent, pour lui faire affichée les infos de Song
+    // On surcharge la méthode displaySongInfo() héritée de la classe abstraite SongComponent, pour lui faire affichée les infos de Song
     @Override
     public void displaySongInfo () {
         System.out.println(getSongName() + getBandName() + getReleaseYear());
